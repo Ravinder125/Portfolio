f545bd9468dd6c194e0aa9a76ae05006d7934635
Ravinder <ravinder729090@gmail.com>
2025-07-30 21:26:10 +0530

    New Technology add "TypeScript"

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,8 +1,9 @@
 import { motion } from "framer-motion"
+import { BiLogoTypescript } from "react-icons/bi"
 import { DiRedis } from "react-icons/di"
 import { FaNodeJs } from "react-icons/fa6"
 import { RiReactjsLine } from "react-icons/ri"
-import { SiExpress, SiMongodb } from "react-icons/si"
+import { SiExpress, SiMongodb, SiTypescript } from "react-icons/si"
 import { TbBrandNextjs } from "react-icons/tb"
 
 const iconVariants = (duration) => ({
@@ -77,6 +78,14 @@ const Technologies = () => {
                     className="p-4"
                 >
                     <DiRedis className="text-7xl text-red-700" />
+                </motion.div>
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(2.5)}
+                    className="p-4"
+                >
+                    <BiLogoTypescript className="text-7xl text-cyan-500" />
                 </motion.div>
             </motion.div>
         </div>
